3b092174299d7939304d2398dab739d0691ad990
Piotr <ciepcin@gmail.com>
2017-09-21 09:43:27 +0200

    Update printObject.js

--- printObject.js
+++ printObject.js
@@ -1,11 +1,39 @@
-(function ( $ ) {
-    
-       $.fn.printObject = function() {
-           var $body = $("body");
-           var html = $($body).html();
-            $($body).empty().append($(this));
-            window.print();
-            $($body).html(html);
-       };
-    
-   }( jQuery ));
+(function ( $ ) 
+{
+   $.fn.printObject = function(params) 
+   {
+       var options = 
+        {
+            header: "",
+            header_style: "",
+            footer: "",
+            footer_style: ""
+        };
+
+        $.extend(options, params);
+        
+        var header = "";
+        var footer = "";
+        
+        if(options.header !== "" && options.header !== null && options.header !== 'undefined')
+            header = "<div style=\""+options.header_style+"\">" + options.header + "</div>";
+        
+        if(options.footer !== "" && options.footer !== null && options.footer !== 'undefined')
+            footer = "<div style=\""+options.footer_style+"\">" + options.footer + "</div>";
+        
+        var printWindow = window.open("", "_blank", "");
+        var head = $("head").html();
+        printWindow.document.open();
+        printWindow.document.write('<html><head>'+head+'</head><body>');
+        printWindow.document.write(header + $(this).html() + footer);
+        printWindow.document.write('</body></html>');
+        printWindow.document.close();
+        printWindow.focus();
+        
+        setTimeout(function() 
+        {
+            printWindow.print();
+            printWindow.close();
+        }, 100);
+   };
+}( jQuery ));
